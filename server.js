ef869a17d1c616d7259513c20c8e9d40a2eda1a1
blueberry <gupta.pratima.3172@gmail.com>
2020-10-03 16:43:23 +0530

    Update server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+module.exports = app;
+
+const PORT = process.env.PORT || 4001;
+
+
+// Mount your existing apiRouter below at the '/api' path.
+const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
+
+// This conditional is here for testing purposes:
+if (!module.parent) { 
+  // Add your code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
